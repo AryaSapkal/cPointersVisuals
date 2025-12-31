0df44d8b7e3f2ff5a62f8cc2b113c50e3352a480
Arya Sapkal <2024asap22@gmail.com>
2025-12-31 14:27:36 -0500

    Fixed highlighting issue by adding .firstChild and other debugging.
    Learned about the concept of nodes, how they're different from elements, how text is a type of node, and how nodes allow for clean DOM representation.

--- script.js
+++ script.js
@@ -4,6 +4,59 @@ let numMemoryCells = 15;
 
 document.addEventListener("DOMContentLoaded", () => {
   //let numMemoryCells = 10;
+
+  function createMemoryCell(cellText){
+    const memoryCell = document.createElement("div");
+    memoryCell.classList.add("memory-cell");
+
+    memoryCell.textContent = cellText;
+  }
+
+  function createMemoryAddress(addressText){
+    const memoryAddress = document.createElement("div");
+    memoryAddress.classList.add("memory-address");
+
+    memoryAddress.textContent = addressText;
+  }
+
+  function createMemoryCellAddress(cellText, addressText){
+    const memoryCellAddress = document.createElement("div");
+    memoryCellAddress.classList.add("memory-cell-address");
+
+    memoryCellAddress.innerHTML.add(createMemoryCell(cellText));
+    memoryCellAddress.innerHTML.add(createMemoryAddress(addressText));
+  }
+/*
+  function createMemoryCellAddresses(count, bytesBetweenEachCell){
+    const memoryCellAddresses = document.createElement("div");
+    memoryCellAddresses.classList.add("memory-cell-addresses");
+
+    for(let i = 0; i < count; i++){
+      
+    }
+  }*/
+
+  function createMemoryBlock(count, parentMemoryBlock, bytesBetweenEachCell){
+    const memoryBlock = parentMemoryBlock;
+    memoryBlock.innerHTML = "";
+
+    //const memoryCellAddresses = document.createElement("div");
+    //memoryCellAddresses.classList.add("memory-cell-addresses");
+
+    for(let i = 0; i < count; i++){
+      const memoryCellAddress = createMemoryCellAddress("", i*bytesBetweenEachCell);
+      memoryCellAddress.classList.add("memory-cell-address");
+
+      //memoryCellAddresses.appendChild(memoryCellAddress);
+      memoryBlock.appendChild(memoryCellAddress);
+    }
+
+    //memoryBlock.appendChild(memoryCellAddresses);
+
+  }
+
+  //-------------------
+
   function createMemoryCells(count, parentMemoryBlock, bytesBetweenEachCell) {
     const memoryBlock = parentMemoryBlock;
 
@@ -12,7 +65,7 @@ document.addEventListener("DOMContentLoaded", () => {
     for (let i = 0; i < count; i++) {
       // Create a cell with class memoryCell and text content of i
       const cell = document.createElement("div");
-      cell.classList.add("memoryCell");
+      cell.classList.add("memory-cell");
       cell.textContent = i*(bytesBetweenEachCell); // Cells can have a varying number of bytes (and corresponding addresses increase amounts) for changeable visual simplicity/complexity
 
       memoryBlock.appendChild(cell);
@@ -40,13 +93,50 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
 
-  createMemoryCells(
+
+
+
+  
+  //const lineOfCode = processNode.querySelector(":scope > p:first-of-type"); // Look for the first paragraph that is a direct child of this div
+  const processNode = document.getElementById("process-block-1");
+  const lineOfCode = processNode.querySelector("#temp");
+  const text = lineOfCode.firstChild;
+
+  if(!CSS.highlights){
+    lineOfCode.textContent = "The CSS custom highlight api is not supported in this browser."
+  }
+  
+  const range = new Range();
+
+  range.setStart(text, 0);
+  range.setEnd(text, text.length);
+
+
+  const highlight = new Highlight(range);
+
+  CSS.highlights.set("highlight-1", highlight); // highlight-1 is a custom identifier of highlight that can be used in CSS
+
+
+
+
+  
+
+
+
+
+
+
+
+
+  createMemoryBlock(numMemoryCells, document.querySelector("#pointers-memory-block"), 1);
+
+  /*createMemoryCells(
     numMemoryCells,
     document.querySelector("#pointers-memory-block"), 1
-  );
+  );*/
   createMemoryCellsAll(
     numMemoryCells,
-    document.querySelectorAll(".files-memory-block"), 1
+    document.querySelectorAll(".files-memory-block"), 4
   );
 
   // createMemoryCells(
